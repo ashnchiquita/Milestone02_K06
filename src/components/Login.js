03ae565f8fbd18bce4ab52b606b709caad6d4bce
Pramaditya Fajri Migfar <89334139+fajrimigfar@users.noreply.github.com>
2022-08-14 15:26:19 +0700

    Update Login.js

--- src/components/Login.js
+++ src/components/Login.js
@@ -2,9 +2,27 @@ import React from "react";
 
 const Login = () => {
   return (
-    <>
-      <h1>Login</h1>
-    </>
+    <div class="main">
+        <div class="logo">
+            <a href="#"><img src="assets/logo.png" alt="logo"></a>
+        </div>
+        <form>
+            <div class="user_input">
+                <input type="text" placeholder="Username" required>
+            </div>
+            <div class="user_input">
+                <input type="password" placeholder="Password" required>
+            </div>
+            <div class="forgot"><p>Lupa Password?</p></div>
+            <input type="submit" value="GO">
+            <div class="signup">
+                Belum punya akun? <a href="#">Sign Up</a>
+            </div>
+        </form>
+    </div>
+    <div class="dino">
+        <img src="assets/dino.jpg" alt="dino">
+    </div>
   );
 };
 
