5abb2a281b42cc4ef371863c49c27e4aa8c0a5dc
sofyanfirdaus <16521144@std.stei.itb.ac.id>
2022-08-15 20:37:02 +0700

    quite a big update

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,12 +1,105 @@
 import React from "react";
-import { Container, Row, Col, Card } from "react-bootstrap";
+import { Container, Row, Col } from "react-bootstrap";
 import streak from "../assets/svg/streak.svg";
 import treasure from "../assets/svg/treasure.svg";
 import heart from "../assets/svg/heart.svg";
 import infty from "../assets/svg/infty.svg";
 import bab1_mtk from "../assets/img/bab1_mtk.png";
-import crown from "../assets/svg/crown.svg";
 import lock from "../assets/svg/lock.svg";
+
+const Progression = (props) => {
+  let prog_svg = (
+    <div style={props.style}>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 500 125"
+        className="progression"
+      >
+        <defs>
+          <linearGradient id="lg">
+            <stop offset="0%" stopColor="#ECC055" />
+            <stop offset={props.progress} stopColor="#ECC055" />
+            <stop offset="0%" stopColor="#C4C4C4" />
+            <stop offset="100%" stopColor="#C4C4C4" />
+          </linearGradient>
+        </defs>
+        <path
+          fill="url(#lg)"
+          stroke="#E5E1E0"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth="10px"
+          d="M115 55L455 55A25 25 180 0 1 455 105L105 105L115 25L85 55L60 5L35 55L5 25L20 105H105"
+        />
+      </svg>
+    </div>
+  );
+  return prog_svg;
+};
+
+const Bab = (props) => {
+  let not_locked_elements = (
+    <>
+      <h2
+        style={{
+          position: "absolute",
+          top: "5%"
+        }}
+      >
+        Bab {props.bab}
+      </h2>
+      <img
+        src={props.logo}
+        alt="logo"
+        className="my-2"
+        style={{
+          maxWidth: "100%",
+          display: "block",
+          width: "70%",
+          objectFit: "contain",
+          position: "absolute",
+          top: "15%"
+        }}
+      />
+      <Progression 
+        progress={props.progress} 
+        style={{
+          position: "absolute",
+          width: "90%",
+          display: "block",
+          bottom: "10%"
+        }}
+      />
+      <a href="/kelas" className="stretched-link" style={{ fontSize: 0 }}>
+        link
+      </a>
+    </>
+  );
+  let locked_elements = (
+    <>
+      <img
+        src={lock}
+        alt="lock"
+        style={{ width: "30%", position: "absolute", top: "30%" }}
+      />
+    </>
+  );
+  return (
+    <Col className="d-flex justify-content-center align-items-center col-lg-3 col-md-4 col-6 mb-3">
+      <div
+        className="shadow mt-3 bab-materi d-flex align-items-center justify-content-center"
+        style={{
+          border: "1px solid #c4c4c4",
+          paddingTop: "120%",
+          position: "relative",
+          boxSizing: "border-box",
+        }}
+      >
+        {props.locked ? locked_elements : not_locked_elements}
+      </div>
+    </Col>
+  );
+};
 
 const Homepage = () => {
   return (
@@ -42,37 +135,10 @@ const Homepage = () => {
           <Col className="col-6 text-end crown-earned">18/40</Col>
         </Row>
         <Row>
-          <Col className="d-flex justify-content-center col-lg-3 col-md-4 col-6 mb-3">
-            <Card className="shadow py-3 mt-3 align-items-center justify-content-center bab-materi">
-              <h2>Bab 1</h2>
-              <img
-                src={bab1_mtk}
-                alt="Bab 1"
-                style={{ width: "70%", objectFit: "cover" }}
-                className="my-2"
-              />
-              <div className="completion">
-                <div className="comp-progress"></div>
-                <img src={crown} alt="crown" />
-              </div>
-              <a href="/kelas" className="stretched-link" style={{fontSize: 0}}>link</a>
-            </Card>
-          </Col>
-          <Col className="d-flex justify-content-center col-lg-3 col-md-4 col-6 mb-3">
-            <Card className="shadow py-3 mt-3 align-items-center justify-content-center bab-materi">
-              <img src={lock} alt="lock" style={{width: "30%"}} />
-            </Card>
-          </Col>
-          <Col className="d-flex justify-content-center col-lg-3 col-md-4 col-6 mb-3">
-            <Card className="shadow py-3 mt-3 align-items-center justify-content-center bab-materi">
-              <img src={lock} alt="lock" style={{width: "30%"}} />
-            </Card>
-          </Col>
-          <Col className="d-flex justify-content-center col-lg-3 col-md-4 col-6 mb-3">
-            <Card className="shadow py-3 mt-3 align-items-center justify-content-center bab-materi">
-              <img src={lock} alt="lock" style={{width: "30%"}} />
-            </Card>
-          </Col>
+            <Bab bab="1" logo={bab1_mtk} progress="30%" />
+            <Bab locked />
+            <Bab locked />
+            <Bab locked />
         </Row>
       </Container>
     </>
