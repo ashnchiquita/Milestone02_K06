9cd801d1d40560f8df80689f01c39d19af0ea790
sofyanfirdaus <16521144@std.stei.itb.ac.id>
2022-08-16 19:27:52 +0700

    update homepage

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,105 +1,13 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
+import JudulPelajaran from "./JudulPelajaran";
+import Bab from "./Bab";
 import streak from "../assets/svg/streak.svg";
 import treasure from "../assets/svg/treasure.svg";
 import heart from "../assets/svg/heart.svg";
 import infty from "../assets/svg/infty.svg";
-import bab1_mtk from "../assets/img/bab1_mtk.png";
-import lock from "../assets/svg/lock.svg";
-
-const Progression = (props) => {
-  let prog_svg = (
-    <div style={props.style}>
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 500 125"
-        className="progression"
-      >
-        <defs>
-          <linearGradient id="lg">
-            <stop offset="0%" stopColor="#ECC055" />
-            <stop offset={props.progress} stopColor="#ECC055" />
-            <stop offset="0%" stopColor="#C4C4C4" />
-            <stop offset="100%" stopColor="#C4C4C4" />
-          </linearGradient>
-        </defs>
-        <path
-          fill="url(#lg)"
-          stroke="#E5E1E0"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          strokeWidth="10px"
-          d="M115 55L455 55A25 25 180 0 1 455 105L105 105L115 25L85 55L60 5L35 55L5 25L20 105H105"
-        />
-      </svg>
-    </div>
-  );
-  return prog_svg;
-};
-
-const Bab = (props) => {
-  let not_locked_elements = (
-    <>
-      <h2
-        style={{
-          position: "absolute",
-          top: "5%"
-        }}
-      >
-        Bab {props.bab}
-      </h2>
-      <img
-        src={props.logo}
-        alt="logo"
-        className="my-2"
-        style={{
-          maxWidth: "100%",
-          display: "block",
-          width: "70%",
-          objectFit: "contain",
-          position: "absolute",
-          top: "15%"
-        }}
-      />
-      <Progression 
-        progress={props.progress} 
-        style={{
-          position: "absolute",
-          width: "90%",
-          display: "block",
-          bottom: "10%"
-        }}
-      />
-      <a href="/kelas" className="stretched-link" style={{ fontSize: 0 }}>
-        link
-      </a>
-    </>
-  );
-  let locked_elements = (
-    <>
-      <img
-        src={lock}
-        alt="lock"
-        style={{ width: "30%", position: "absolute", top: "30%" }}
-      />
-    </>
-  );
-  return (
-    <Col className="d-flex justify-content-center align-items-center col-lg-3 col-md-4 col-6 mb-3">
-      <div
-        className="shadow mt-3 bab-materi d-flex align-items-center justify-content-center"
-        style={{
-          border: "1px solid #c4c4c4",
-          paddingTop: "120%",
-          position: "relative",
-          boxSizing: "border-box",
-        }}
-      >
-        {props.locked ? locked_elements : not_locked_elements}
-      </div>
-    </Col>
-  );
-};
+import mtkipa from "../assets/img/mtkipa.png";
+import pkn from "../assets/img/pkn.png";
 
 const Homepage = () => {
   return (
@@ -130,15 +38,26 @@ const Homepage = () => {
         </Row>
       </Container>
       <Container fluid className="mt-4 px-4 px-lg-5">
-        <Row className="align-items-center px-2">
-          <Col className="col-6 h1">Matematika</Col>
-          <Col className="col-6 text-end crown-earned">18/40</Col>
-        </Row>
+        <JudulPelajaran judul="Matematika" mahkota={18} batasMahkota={40} />
         <Row>
-            <Bab bab="1" logo={bab1_mtk} progress="30%" />
-            <Bab locked />
-            <Bab locked />
-            <Bab locked />
+          <Bab bab="1" logo={mtkipa} progress={18/40*100} />
+          <Bab locked />
+          <Bab locked />
+          <Bab locked />
+        </Row>
+        <JudulPelajaran judul="PKN" mahkota={36} batasMahkota={36} />
+        <Row>
+          <Bab bab="1" logo={pkn} progress={36*36*100} />
+          <Bab locked />
+          <Bab locked />
+          <Bab locked />
+        </Row>
+        <JudulPelajaran judul="IPA" mahkota={10} batasMahkota={51} />
+        <Row>
+          <Bab bab="1" logo={mtkipa} progress={10/51*100} />
+          <Bab locked />
+          <Bab locked />
+          <Bab locked />
         </Row>
       </Container>
     </>
