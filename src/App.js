c0910551b332bf5526d911dfb10d859cc4cc9ef6
Margaretha Olivia <margarethaolivia41@gmail.com>
2022-08-16 09:16:21 +0700

    edit latihan (jawaban onClick logic)

--- src/App.js
+++ src/App.js
@@ -8,28 +8,21 @@ import Profil from "./components/Profil";
 import Login from "./components/Login";
 import Tantangan from "./components/Tantangan";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Jawabanbenar from "./components/Jawabanbenar";
-import Jawabansalaha from "./components/Jawabansalaha";
-import Jawabansalahb from "./components/Jawabansalahb";
-import Jawabansalahc from "./components/Jawabansalahc";
 
 function App() {
   return (
     <Router>
       <Navbar />
-      {/* <Container> */} {/* dihapus karena kalo pake ini jadi gak bisa full width */}
-        <Routes>
-          <Route path="/" element={<Homepage />} />
-          <Route path="/kelas" element={<Kelas />} />
-          <Route path="/latihan" element={<Latihan />} />
-          <Route path="/tantangan" element={<Tantangan />} />
-          <Route path="/profil" element={<Profil />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/jawabanbenar" element={<Jawabanbenar />} />
-          <Route path="/jawabansalaha" element={<Jawabansalaha />} />
-          <Route path="/jawabansalahb" element={<Jawabansalahb />} />
-          <Route path="/jawabansalahc" element={<Jawabansalahc />} />
-        </Routes>
+      {/* <Container> */}{" "}
+      {/* dihapus karena kalo pake ini jadi gak bisa full width */}
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/kelas" element={<Kelas />} />
+        <Route path="/latihan" element={<Latihan />} />
+        <Route path="/tantangan" element={<Tantangan />} />
+        <Route path="/profil" element={<Profil />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
       {/* </Container> */}
     </Router>
   );
